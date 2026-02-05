f51b747de4d76d50f01e3ed7e90ff0e5de864b79
adi-943 <aditisatishreddy@gmail.com>
2026-02-05 01:13:58 -0400

    Add files via upload

---
+++ CP_UNIT3/Program3b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+struct Student {
+int id;
+char name[20];
+};
+int main() {
+struct Student s = {101, "Anu"};
+struct Student *ptr = &s;
+printf("ID: %d\n", ptr->id);
+printf("Name: %s\n", ptr->name);
+return 0;
+}
+output
+ID:101
+Name:Anu
